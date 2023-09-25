2eedbebcbf34a7130c2c09fd0fde4d645545a185
chenmo <chenmo1212@qq.com>
2023-09-25 14:40:20 +0100

    feat: add sideDrawer width attr

--- src/core/widgets/SideDrawer.tsx
+++ src/core/widgets/SideDrawer.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
+interface WrapperProps {
+  width?: string;
+}
+
 const Container = styled.div`
   background: #222;
   padding: 0.25rem 0.25rem 0.25rem 1rem;
   height: 100%;
 `;
 
-const Wrapper = styled.div`
+const Wrapper = styled.div<WrapperProps>`
   height: 100vh;
   overflow-y: auto;
-  width: 380px;
   background: #222;
+  width: ${(props) => props.width || '380px'};
 
   &.hide {
     display: none;
   }
 `;
 
-export function SideDrawer({ isShown, children }: any) {
+export function SideDrawer({ isShown, width, children }: any) {
   return (
-    <Wrapper className={isShown ? '' : 'hide'}>
+    <Wrapper className={isShown ? '' : 'hide'} width={width}>
       <Container>{children}</Container>
     </Wrapper>
   );
