4436545db905c980a468d48ddc44ad50446b80e3
ChenMo1212 <chenmo1212@qq.com>
2025-09-18 16:12:15 +0100

    remove ts

--- next.config.js
+++ next.config.js
@@ -7,6 +7,6 @@ module.exports = {
     domains: ['img.icons8.com'],
   },
   eslint: {
-    dirs: ['src/pages', 'src/core', 'src/stores', 'src/templates', 'src/home'],
+    dirs: ['src/pages', '../core', '../../../stores', 'src/templates', 'src/home'],
   },
 };
